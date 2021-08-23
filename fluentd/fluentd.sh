0081a4d8bc37b34e78271267c9d7ca33ad4ae2e3
ToasT1ng <jrainboww9@gmail.com>
2021-08-23 22:15:40 +0900

    able to tail

--- fluentd/fluentd.sh
+++ fluentd/fluentd.sh
@@ -1,6 +1,71 @@
 #!/bin/bash
 
 SERVICE="fluentd"
+PWD="$(pwd)"
+LOG_PATH=""
+FLUENTD_POS_PATH="${PWD}/pos"
+
+function print_usage() {
+/bin/cat << EOF
+
+	Usage :
+	    $0 [ -lp log_path ] [ -fp fluentd_pos_file_path ]
+	Option :
+	    -lp, --log_path, --log                    your log's path
+	    -fp, --fluentd_pos_path                   your fluentd .pos file path
+	    -h, --help                                print usages
+
+
+EOF
+}
+
+while (( "$#" )); do
+    case "$1" in
+        -lp|--log_path|--log)
+            if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+                LOG_PATH=$2
+                shift 2
+            else
+                echo "Error: Argument for $1 is missing" >&2
+                exit 1
+            fi
+            ;;
+        -ldp|--log_dev_path|--log_dev)
+            if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+                LOG_DEV_PATH=$2
+                shift 2
+            else
+                echo "Error: Argument for $1 is missing" >&2
+                exit 1
+            fi
+            ;;
+        -fp|--fluentd_pos_path)
+            if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+                FLUENTD_POS_PATH=$2
+                shift 2
+            else
+                echo "Error: Argument for $1 is missing" >&2
+                exit 1
+            fi
+            ;;
+        -h|--help)
+            print_usage
+            exit 1
+            ;;
+        *)
+            echo "Error: Arguments with not proper flag: $1" >&2
+            echo "$0 -h for help message" >&2
+            print_usage
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z "$LOG_PATH" ]]
+then
+        echo "You need to input log_path. If you want more information, try   bash $0 -h"
+        exit 1
+fi
 
 CONTAINER_ID=$(sudo docker ps -a -f "name=${SERVICE}" -q)
 if [ -z "$CONTAINER_ID" ] ; then
@@ -15,6 +80,6 @@ fi
 echo "start"
 {
   sudo docker build -f Dockerfile -t toast1ng/${SERVICE} .
-	sudo docker run -d -p 8888:8888 -e FLUENTD_CONF=fluentd.conf --name ${SERVICE} toast1ng/${SERVICE}
-	# -v /home/ubuntu/fluentd/buffer:/var/buffer -v /home/ubuntu/fluentd/pos:/var/pos -v /home/ubuntu/spring/log:/var/log -v /home/ubuntu/spring/log_dev:/var/log_dev
+	sudo docker run -d -e FLUENTD_CONF=fluentd.conf -v ${FLUENTD_POS_PATH}:/var/pos -v ${LOG_PATH}:/var/log --name ${SERVICE} toast1ng/${SERVICE}
+	# -v /home/ubuntu/fluentd/buffer:/var/buffer
 }
